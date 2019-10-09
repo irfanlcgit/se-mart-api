8dab8bc86fccc82f6cdcffa8238f0fb97e9c2aba
irfan <mirfan@leadconcept.com>
2019-10-09 18:30:23 +0500

    initializ project

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// create express app
+const app = express();
+
+// declare axios for making http requests
+const axios = require('axios');
+const API = 'https://jsonplaceholder.typicode.com';
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// Configuring the database
+/*const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});*/
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    database : 'paylessdemo',
+    user     : 'root',
+    password : 'irfanlc',
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('Connected as id ' + connection.threadId);
+});
+
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+// Require Notes routes
+require('./app/routes/note.routes.js')(app);
+
+app.post('/api/test', (req, res) => {
+
+	connection.query('SELECT * from user LIMIT 2', function(err, rows, fields) {
+		connection.end();
+	  	if (!err)
+	  		res.status(200).json({"result": rows});
+	  	else
+	    	res.status(500).send({"message": err.sqlMessage});
+	});
+    
+    /*axios.post(`https://partnerlink.fastpay.co.id:4343/devel/cek_ip.php`)
+    .then(posts => {
+      res.status(200).json(posts.data);
+    })
+    .catch(error => {
+      res.status(500).send(error)
+    });*/
+
+});
+
+// listen for requests
+app.listen(3001, () => {
+    console.log("Server is listening on port 3001");
+});
