5573cf2330f7f99a7d0a0ded63f31c5b675c7f99
Muhammad Irfan <link2mirfan@gmail.com>
2019-11-30 21:32:04 +0500

    30-11-2019

--- app/controllers/ppob.controller.js
+++ app/controllers/ppob.controller.js
@@ -169,7 +169,7 @@ exports.mobileCredit = (req, res) => {
             var new_transection = new Transection({
                 order_id: random(),
                 bill_id: (req.body.type === "internet")? 2 : 1,
-                ref_customer_id: req.body.ref_customer_id,
+                ref_customer_id: req.body.ref_customer_id.replace(/\\"/g, '"'),
                 product_code: req.body.product_code,
                 area_code: null,
                 phone: req.body.phone_number,
